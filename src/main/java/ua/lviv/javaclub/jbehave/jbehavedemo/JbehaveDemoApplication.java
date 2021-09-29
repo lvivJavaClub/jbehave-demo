584e7737e93a08071093f038517730281c2c1dbd
ibanadiga <ibanadiga@lohika.com>
2021-09-29 18:37:33 +0300

    init

---
+++ src/main/java/ua/lviv/javaclub/jbehave/jbehavedemo/JbehaveDemoApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.javaclub.jbehave.jbehavedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbehaveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbehaveDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/lviv/javaclub/jbehave/jbehavedemo/JbehaveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.javaclub.jbehave.jbehavedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbehaveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
